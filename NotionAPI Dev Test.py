87fcf821197778a417b476f8a7633ccb3aa05d11
adminYBL <admin@yourbottomline.ca>
2024-05-08 13:57:47 -0400

    Update NotionAPI Dev Test.py

--- NotionAPI Dev Test.py
+++ NotionAPI Dev Test.py
@@ -3,6 +3,74 @@ import requests
 from datetime import datetime, timezone
 #notion api example
 #full version on github
-NOTION_TOKEN ="secret_64ZRB08voTqHlO5dHnOrdqxNgdlh6voBE3gleMj10yk"
+NOTION_TOKEN ="secret_qWL2UEpThCmEcaNHTfAW2U56dSL0BY9SlvTxzK38aDe"
 #share database with integration on Notion
-DATABASE_ID = "a9233401a0a04cb3b931562069114104"
+DATABASE_ID = "52e1c370bdfd45428f88833190635e3e"
+
+headers = {
+    "Authorization": "Bearer " + NOTION_TOKEN,
+    "Content-Type": "application/json",
+    "Notion-Version": "2022-06-28",
+}
+
+def create_page(data: dict):
+    create_url = "https://api.notion.com/v1/pages"
+
+    payload = {"parent": {"database_id": DATABASE_ID}, "properties": data}
+
+    res = requests.post(create_url, headers=headers, json=payload)
+    # print(res.status_code)
+    return res
+
+from datetime import datetime, timezone
+
+title = "Test Title"
+description = "Test Description"
+published_date = datetime.now().astimezone(timezone.utc).isoformat()
+data = {
+    "URL": {"title": [{"text": {"content": description}}]},
+    "Title": {"rich_text": [{"text": {"content": title}}]},
+    "Published": {"date": {"start": published_date, "end": None}}
+}
+
+create_page(data)
+
+def get_pages(num_pages=None):
+    """
+    If num_pages is None, get all pages, otherwise just the defined number.
+    """
+    url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
+
+    get_all = num_pages is None
+    page_size = 100 if get_all else num_pages
+
+    payload = {"page_size": page_size}
+    response = requests.post(url, json=payload, headers=headers)
+
+    data = response.json()
+
+    # Comment this out to dump all data to a file
+    # import json
+    # with open('db.json', 'w', encoding='utf8') as f:
+    #    json.dump(data, f, ensure_ascii=False, indent=4)
+
+    results = data["results"]
+    while data["has_more"] and get_all:
+        payload = {"page_size": page_size, "start_cursor": data["next_cursor"]}
+        url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
+        response = requests.post(url, json=payload, headers=headers)
+        data = response.json()
+        results.extend(data["results"])
+
+    return results
+
+
+pages = get_pages()
+
+for page in pages:
+    page_id = page["id"]
+    props = page["properties"]
+    url = props["URL"]["title"][0]["text"]["content"]
+    title = props["Title"]["rich_text"][0]["text"]["content"]
+    published = props["Published"]["date"]["start"]
+    published = datetime.fromisoformat(published)
