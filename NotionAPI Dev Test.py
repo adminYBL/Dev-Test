1a90dc635bb11e6db5a76f27cabcbdac62687266
adminYBL <admin@yourbottomline.ca>
2024-05-08 12:20:41 -0400

    Add files via upload

---
+++ NotionAPI Dev Test.py
@@ -0,0 +1,8 @@
+#set up Notion API on Python
+import requests
+from datetime import datetime, timezone
+#notion api example
+#full version on github
+NOTION_TOKEN ="secret_64ZRB08voTqHlO5dHnOrdqxNgdlh6voBE3gleMj10yk"
+#share database with integration on Notion
+DATABASE_ID = "a9233401a0a04cb3b931562069114104"
